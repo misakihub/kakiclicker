7536e51e87e98b37b362ba7b897e505127497d43
misakihub <yudu.pop.nnn@gmail.com>
2021-04-11 23:11:07 +0900

    console.logに出力できた

--- src/App.js
+++ src/App.js
@@ -2,22 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const hantdleClick =()=>{console.log("!!!!")}
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          牡蠣クリッカー
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img onClick={
+            ()=>{hantdleClick()}
+          } src="/kaki.png" />
+
+
+
       </header>
+
     </div>
   );
 }
