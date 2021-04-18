db13790ea0decd0a9097d45823840aaa4781b027
misakihub <yudu.pop.nnn@gmail.com>
2021-04-18 20:13:47 +0900

    ‘カウントできた’

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
-function App() {
-  const hantdleClick =()=>{console.log("!!!!")}
+function App() { 
+   // Declare a new state variable, which we'll call "count"
+const [count, setCount] = useState(0);
+const hantdleClick =() => setCount(count+1);
+  
   return (
     <div className="App">
       <header className="App-header">
         
-        <p>
+        <h1>
           牡蠣クリッカー
-        </p>
+        </h1>
+        <p>牡蠣の数{count}</p>
         <img onClick={
             ()=>{hantdleClick()}
           } src="/kaki.png" />
-
 
 
       </header>
